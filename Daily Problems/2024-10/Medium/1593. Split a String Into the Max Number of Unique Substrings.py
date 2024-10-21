cd6d89466cf1d4d330f6a83b8ee0456ed1b1b885
MichaelHeUVA <theasiankid669@gmail.com>
2024-10-20 20:43:50 -0400

    Completed daily problem 10/21/2024

---
+++ Daily Problems/2024-10/Medium/1593. Split a String Into the Max Number of Unique Substrings.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/split-a-string-into-the-max-number-of-unique-substrings/description/
+
+
+class Solution:
+    def maxUniqueSplit(self, s: str) -> int:
+        n = len(s)
+        max_length = 0
+
+        def backtrack(index, path):
+            nonlocal max_length
+            if index == n:
+                max_length = max(max_length, len(path))
+
+            for i in range(index, n):
+                if s[index : i + 1] not in path:
+                    path.add(s[index : i + 1])
+                    backtrack(i + 1, path)
+                    path.remove(s[index : i + 1])
+
+        backtrack(0, set())
+        return max_length
