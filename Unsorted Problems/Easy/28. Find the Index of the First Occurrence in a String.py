fe45a6051358ca88cfc795a01fcde1617363414d
MichaelHeUVA <theasiankid669@gmail.com>
2024-10-29 16:49:54 -0400

    Completed 28

---
+++ Unsorted Problems/Easy/28. Find the Index of the First Occurrence in a String.py
@@ -0,0 +1,6 @@
+# https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/description/
+
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        return haystack.find(needle)
