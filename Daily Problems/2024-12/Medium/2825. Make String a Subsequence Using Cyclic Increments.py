b81d234c4745db8a87a67eee0b7bdea543705832
MichaelHeUVA <theasiankid669@gmail.com>
2024-12-03 19:43:45 -0500

    Completed daily 12/4/2024

---
+++ Daily Problems/2024-12/Medium/2825. Make String a Subsequence Using Cyclic Increments.py
@@ -0,0 +1,18 @@
+# 2825. Make String a Subsequence Using Cyclic Increments
+
+
+class Solution:
+    def canMakeSubsequence(self, str1: str, str2: str) -> bool:
+        if len(str2) > len(str1):
+            return False
+        pointer = 0
+        for i in range(len(str1)):
+            if pointer >= len(str2):
+                return True
+            if (
+                str1[i] == str2[pointer]
+                or chr((ord(str1[i]) - ord("a") + 1) % 26 + ord("a")) == str2[pointer]
+            ):
+                pointer += 1
+
+        return pointer == len(str2)
