907e8da2e93834e9db772e762e3e21bac0f874aa
MichaelHeUVA <theasiankid669@gmail.com>
2024-12-05 20:57:53 -0500

    Completed weekly 12/01/2024

---
+++ Weekly Premium/2024-12/Medium/277. Find the Celebrity.py
@@ -0,0 +1,25 @@
+# https://leetcode.com/problems/find-the-celebrity/description/?envType=weekly-question&envId=2024-12-01
+# The knows API is already defined for you.
+# return a bool, whether a knows b
+def knows(a: int, b: int) -> bool:
+    pass
+
+
+class Solution:
+    def findCelebrity(self, n: int) -> int:
+        self.n = n
+        celebrity_candidate = 0
+        for i in range(1, n):
+            if knows(celebrity_candidate, i):
+                celebrity_candidate = i
+        if self.is_celebrity(celebrity_candidate):
+            return celebrity_candidate
+        return -1
+
+    def is_celebrity(self, i):
+        for j in range(self.n):
+            if i == j:
+                continue
+            if knows(i, j) or not knows(j, i):
+                return False
+        return True
