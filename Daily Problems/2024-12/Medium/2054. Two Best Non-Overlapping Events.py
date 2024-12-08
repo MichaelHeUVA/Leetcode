cc88c6b381047ee660ea7b9779844dac6d0f0f6d
MichaelHeUVA <theasiankid669@gmail.com>
2024-12-08 00:47:16 -0500

    Completed daily 12/8/2024

---
+++ Daily Problems/2024-12/Medium/2054. Two Best Non-Overlapping Events.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/two-best-non-overlapping-events/description/
+from typing import List
+
+
+class Solution:
+    def maxTwoEvents(self, events: List[List[int]]) -> int:
+        times = []
+
+        for start, end, value in events:
+            times.append((start, "start", value))
+            times.append((end + 1, "end", value))
+
+        times.sort()
+        output = 0
+        max_value = 0
+        for _, start_or_end, value in times:
+            if start_or_end == "start":
+                output = max(output, value + max_value)
+            else:
+                max_value = max(max_value, value)
+        return output
